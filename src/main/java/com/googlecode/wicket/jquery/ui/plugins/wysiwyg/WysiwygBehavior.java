4c36eb5b1d65a0aa1af813f435daa0b6b940783e
andunslg <andunslg@gmail.com>
2013-05-27 18:51:35 +0530

    Formatting resources

--- src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/WysiwygBehavior.java
+++ src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/WysiwygBehavior.java
@@ -6,13 +6,13 @@ import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 public class WysiwygBehavior extends JQueryBehavior{
-	private static final JavaScriptResourceReference JS1=new JavaScriptResourceReference(WysiwygBehavior.class,"bootstrap-wysiwyg.js");
-	private static final JavaScriptResourceReference JS2=new JavaScriptResourceReference(WysiwygBehavior.class,"bootstrap.min.js");
-	private static final JavaScriptResourceReference JS3=new JavaScriptResourceReference(WysiwygBehavior.class,"jquery.hotkeys.js");
-	private static final JavaScriptResourceReference JS4=new JavaScriptResourceReference(WysiwygBehavior.class,"prettify.js");
-	private static final CssResourceReference CSS1=new CssResourceReference(WysiwygBehavior.class,"bootstrap-combined.no-icons.min.css");
-	private static final CssResourceReference CSS2=new CssResourceReference(WysiwygBehavior.class,"bootstrap-responsive.min.css");
-	private static final CssResourceReference CSS3=new CssResourceReference(WysiwygBehavior.class,"font-awesome.css");
+	private static final JavaScriptResourceReference JS1=new JavaScriptResourceReference(WysiwygBehavior.class,"js/bootstrap-wysiwyg.js");
+	private static final JavaScriptResourceReference JS2=new JavaScriptResourceReference(WysiwygBehavior.class,"js/bootstrap.min.js");
+	private static final JavaScriptResourceReference JS3=new JavaScriptResourceReference(WysiwygBehavior.class,"js/jquery.hotkeys.js");
+	private static final JavaScriptResourceReference JS4=new JavaScriptResourceReference(WysiwygBehavior.class,"js/prettify.js");
+	private static final CssResourceReference CSS1=new CssResourceReference(WysiwygBehavior.class,"css/bootstrap-combined.no-icons.min.css");
+	private static final CssResourceReference CSS2=new CssResourceReference(WysiwygBehavior.class,"css/bootstrap-responsive.min.css");
+	private static final CssResourceReference CSS3=new CssResourceReference(WysiwygBehavior.class,"css/font-awesome.css");
 
 	public WysiwygBehavior(String selector, String method){
 		super(selector,method);
